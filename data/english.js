d96b3584e20c4f79e6bda02bc72db95674960887
fernandomsilva <fernandomsilva@nyu.edu>
2018-08-29 20:28:29 -0300

    adding deck selection page

---
+++ data/english.js
@@ -0,0 +1,23 @@
+var	english_data =
+	[
+		{
+			"title": "Jurassic Park", 
+			"category": "Movie"
+		},
+		{
+			"title": "Fresh Prince of Bel Air",
+			"category": "TV"
+		},
+		{
+			"title": "Mario Odyssey",
+			"category": "Game"
+		},
+		{
+			"title": "Monalisa",
+			"category": "Art"
+		},
+		{
+			"title": "Book of Mormon",
+			"category": "Play/Musical"
+		}
+	]
